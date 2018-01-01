faec78507fd2854407d22665bbd8654a8020942e
wffeige <wffeige@126.com>
2018-01-01 22:38:51 +0800

    20180101-02

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render
 from django.views.generic import View
 from models import CourseOrg,CityDict
 
+from django.shortcuts import render_to_response
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 class OrgView(View):
     '''
     课程机构列表功能
@@ -17,9 +20,18 @@ class OrgView(View):
 
         #城市
         all_citys = CityDict.objects.all()
+
+        #对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 2,request=request)
+        orgs = p.page(page)
+
         return render(request,'org-list.html',{
-            "all_orgs":all_orgs,
+            "all_orgs":orgs,
             "all_citys":all_citys,
             "org_nums":org_nums
         })
-
